bd418c448308d37af8a1054db3eccebdd7609b0e
serpelbr <serpelbr@gmail.com>
2018-09-19 13:07:50 -0300

    Initial commit

---
+++ src/TiposPrimitivosValores.java
@@ -0,0 +1,16 @@
+public class TiposPrimitivosValores {
+    public static void main(String[] args){
+        int soma = 5 + 5;
+        System.out.println("Soma 5+5: "+soma);
+
+        int subtracao = 10 - 3;
+        System.out.println("Subtração 10-3: "+subtracao);
+
+        long multiplicacao = 110 * 525;
+        System.out.println("Multiplicação 110 * 525: "+multiplicacao);
+
+        int resto = 100 % 30;
+        System.out.println("Resto 100 / 30: " + resto);
+
+    }
+}
